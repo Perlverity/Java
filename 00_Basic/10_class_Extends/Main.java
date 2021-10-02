19b6643018b09ac1430bcff7b6098979fd5e9bac
Perlverity <snowman.ss.6124@gmail.com>
2021-10-02 18:46:23 +0900

    Java Day04

---
+++ 00_Basic/10_class_Extends/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // SuperHero sh = new SuperHero();
+        // sh.run();
+
+        // Hero h = new Hero();
+        // h.run();
+
+        // Matango m = new Matango();
+        // sh.attack(m);
+
+        Weapon w = new Weapon();
+        System.out.println(w.name + "が該当");
+    }
+}
---
+++ 00_Basic/10_class_Extends/Practice/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        PoisonMatango pm = new PoisonMatango('A');
+        Hero h = new Hero();
+        pm.attack(h);
+    }
+}
